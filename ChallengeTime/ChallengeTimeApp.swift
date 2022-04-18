4fb14d4aeec4d4c205518f9b070770758096eb37
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2022-04-18 14:56:35 -0300

    Initial Commit

---
+++ ChallengeTime/ChallengeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTimeApp.swift
+//  ChallengeTime
+//
+//  Created by Thiago Lourenço on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
