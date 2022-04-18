d1292e427b419c38935194c19efb78383e51a139
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2022-04-18 15:42:11 -0300

    first commit

--- ChallengeTime/ContentView.swift
+++ ChallengeTime/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Challenge Time: ").font(.system(size: 32)).bold().multilineTextAlignment(.center)
+
+            HStack {
+                Rectangle().frame(width: 50, height: 50).foregroundColor(.blue)
+                Circle().frame(width: 50, height: 50).foregroundColor(.red)
+                Ellipse().frame(width: 50, height: 30).foregroundColor(.green)
+                Capsule().frame(width: 50, height: 25)
+                Circle().frame(width: 50, height: 50).foregroundColor(.yellow)
+            }.padding()
+            
+            HStack {
+                Text("Quadrado").font(.system(size: 14)).foregroundColor(.blue)
+                Text("Circulo").font(.system(size: 14)).foregroundColor(.red)
+                Text("Elipse").font(.system(size: 14)).foregroundColor(.green)
+                Text("Capsula").font(.system(size: 14)).foregroundColor(.black)
+                Text("Borda").font(.system(size: 14)).foregroundColor(.yellow).bold()
+            }.padding()
+            
+            HStack {
+                Image(systemName: "cloud.sun.rain.fill").font(.system(size: 40)).foregroundStyle(.gray, .yellow, .blue)
+                Image(systemName: "cloud.drizzle.fill").font(.system(size: 40)).foregroundStyle(.gray, .red)
+                Image(systemName: "cloud.rain.fill").font(.system(size: 40)).foregroundStyle(.gray, .green)
+                Image(systemName: "cloud.heavyrain.fill").font(.system(size: 40)).foregroundStyle(.gray, .black)
+                Image(systemName: "cloud.hail.fill").font(.system(size: 40)).foregroundStyle(.gray, .yellow)
+            }.padding()
+            
+            HStack {
+                VStack {
+                    Image(systemName: "cloud.snow.fill").font(.system(size: 30)).foregroundStyle(.gray, .blue)
+                    Text("Nevando").foregroundColor(.blue).padding(.top, .leastNonzeroMagnitude)
+                }
+                
+                VStack {
+                    Image(systemName: "cloud.drizzle.fill").font(.system(size: 30)).foregroundStyle(.gray, .red)
+                    Text("pouco").foregroundColor(.blue).padding(.top, .leastNonzeroMagnitude)
+                }
+                
+                VStack {
+                    Image(systemName: "cloud.rain.fill").font(.system(size: 30)).foregroundStyle(.gray, .green)
+                    Text("medio").foregroundColor(.blue).padding(.top, .leastNonzeroMagnitude)
+                }
+                
+                VStack {
+                    Image(systemName: "cloud.heavyrain.fill").font(.system(size: 30)).foregroundStyle(.gray, .black)
+                    Text("Forte").foregroundColor(.blue).padding(.top, .leastNonzeroMagnitude)
+                }
+                
+                VStack {
+                    Image(systemName: "cloud.hail.fill").font(.system(size: 30)).foregroundStyle(.gray, .black)
+                    Text("Granizo").foregroundColor(.blue).padding(.top, .leastNonzeroMagnitude)
+                }
+            }
+        }
+      
     }
 }
 
